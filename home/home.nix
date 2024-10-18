6f7ccd0e52a09eacffcf65e7b94f387aa18a894f
RKest <max.ind@o2.pl>
2024-10-18 22:45:18 +0200

    waybar config

--- home/home.nix
+++ home/home.nix
@@ -15,6 +15,10 @@
     "${config.xdg.configHome}/hypr/hyprland.conf".source = config.lib.file.mkOutOfStoreSymlink "/home/max/.dotfiles/hyprland.conf";
     "${config.xdg.configHome}/alacritty/alacritty.toml".source = config.lib.file.mkOutOfStoreSymlink "/home/max/.dotfiles/alacritty.toml";
     ".zshrc".source = config.lib.file.mkOutOfStoreSymlink "/home/max/.dotfiles/zsh/.zshrc";
+    "${config.xdg.configHome}/waybar" = {
+    	source = config.lib.file.mkOutOfStoreSymlink "/home/max/.dotfiles/waybar";
+	recursive = true;
+    };
   };
 
   home.sessionVariables = {
