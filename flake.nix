da134bec405c09fc42b3b1db274e3ebefd2844bf
RKest <max.ind@o2.pl>
2024-10-20 02:13:03 +0200

    Only having home manager stuff here

--- flake.nix
+++ flake.nix
@@ -1,30 +1,29 @@
 {
-  description = "System configuration nix flake";
+  description = "Home Manager configuration of max";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
-
-  outputs = inputs:
-  let 
-    lib = inputs.nixpkgs.lib;
-  in {
-    nixosConfigurations.maxlap = lib.nixosSystem {
-      specialArgs = { inherit inputs; };
-      system = "x86_64-linux";
-      modules = [ 
-	./hardware/lap.nix
-        ./configuration.nix 
-      ];
-    };
-
-    nixosConfigurations.maxpc = lib.nixosSystem {
-      specialArgs = { inherit inputs; };
-      system = "x86_64-linux";
-      modules = [ 
-	./hardware/pc.nix
-        ./configuration.nix 
-      ];
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations."max" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
 }
